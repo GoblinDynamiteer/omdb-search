8ed4a07cbe44f3dcd09153ecc37c3cd73f89ae1f
Johan Kämpe <kampe.johan@gmail.com>
2017-10-22 22:57:57 +0200

    List episodes for show

--- omdb-searcher.py
+++ omdb-searcher.py
@@ -44,17 +44,65 @@ class Show:
         self.genre = json_data["Genre"]
         self.actors = json_data["Actors"]
         self.runtime = json_data["Runtime"]
-        self.seasons = json_data["totalSeasons"]
+        self.season_count = json_data["totalSeasons"]
+        self.episode_count = 0
         self.country = json_data["Country"]
+        self.seasons = None
+
+        self.__find_episodes()
+
     def to_string(self):
         print("Show: " + self.title + " (" + self.year +")")
         print(self.country)
-        print(self.seasons + " seasons")
+        print(self.season_count + " seasons")
         print(self.imdb_id)
         print(self.genre)
         print("Runtime: " + self.runtime)
         print("IMDb rating: " + self.imdb_rating)
         print("Actors: " + self.actors)
+
+    def list_episodes(self):
+        if self.seasons == None:
+            self.__find_episodes()
+        for season in self.seasons:
+            for episode in season.episodes:
+                episode.to_string()
+
+    def __find_episodes(self):
+        self.seasons = []
+        for i in range(0, int(self.season_count)):
+            url = site + "?apikey=" + api_key + "&i=" + self.imdb_id + \
+                "&season=" + str(i+1)
+            try:
+                response = urllib.request.urlopen(url).read().decode("utf-8")
+                json_data = json.loads(response)
+                self.seasons.append(Season(json_data, i+1))
+                self.episode_count += self.seasons[i].episode_count
+            except:
+                pass
+
+
+class Season:
+    def __init__(self, json_data, season_number):
+        self.episode_count = len(json_data["Episodes"])
+        self.season_number = season_number
+        self.episodes = []
+        for i in range(0, int(self.episode_count)):
+            title = json_data["Episodes"][i]["Title"]
+            date = json_data["Episodes"][i]["Released"]
+            self.episodes.append(Episode(title, i+1, self.season_number, date))
+
+class Episode:
+    def __init__(self, title, number, season_number, release_date):
+        self.number = number
+        self.title = title
+        self.season_number = season_number
+        self.release_date = release_date
+    def to_string(self):
+        print("S" + "%02d" % self.season_number + "E" + \
+            "%02d" % self.number + ": " + self.title + \
+            " (" + self.release_date + ")")
+
 
 site = "http://www.omdbapi.com"
 
@@ -95,3 +143,6 @@ if json_data["Type"] == "movie":
     data = Movie(json_data)
 
 data.to_string()
+if hasattr(data, 'list_episodes') and False:
+    print("\nEpisodes (" + str(data.episode_count) + ")")
+    data.list_episodes()
