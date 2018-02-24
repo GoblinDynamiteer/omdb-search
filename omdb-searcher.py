933f87d1d6f9c0ba4859a7ae22105b9e2799076e
Johan Kämpe <kampe.johan@gmail.com>
2018-02-24 19:14:30 +0100

    omdb: Renamed functions
    Signed-off-by: Johan Kämpe <kampe.johan@gmail.com>

--- omdb-searcher.py
+++ omdb-searcher.py
@@ -8,32 +8,32 @@ parser = argparse.ArgumentParser(description='OMDb search')
 parser.add_argument('-api', dest='api_key', type=str, help='API key (Required)')
 parser.add_argument('query', type=str, help='Search query')
 parser.add_argument('-year', dest='search_year', help='Year')
-parser.add_argument('-type', dest='search_type', help='Type: movie, series, episode')
+parser.add_argument('-type', dest='search_type', help='get_type: movie, series, episode')
 parser.add_argument('-output', dest='output', \
     help='Output: full, full_noep, imdb, title, year, runtime, actors, genre, episode_list, episode_count') # -o works
 args = parser.parse_args()
 
-search = omdb.OMDb(search_string=args.query, search_type=args.search_type, api_key=args.api_key, search_year=args.search_year)
+search = omdb.omdb_search(search_string=args.query, search_type=args.search_type, api_key=args.api_key, search_year=args.search_year)
 
-if search.JSON() == False:
+if search.get_json() == None:
     print("Error searching for " + args.query)
-    print("String Generated: " + search.GetSearchURL())
+    print("String Generated: " + search.get_url())
     sys.exit()
 else:
-    if search.Type() == "series":
-        data = omdb.Show(search)
-    elif search.Type() == "movie":
-        data = omdb.Movie(search)
+    if search.get_type() is "series":
+        data = omdb.omdb_tv_data(search)
+    elif search.get_type() is "movie":
+        data = omdb.omdb_movie_data(search)
     else:
-        print("Error searching for " + args.query)
-        print("String Generated: " + search.GetSearchURL())
+        print("Could not get type for " + args.query)
+        print("String Generated: " + search.get_url())
         sys.exit()
 
 # Output
 if args.output == "full" or args.output == "full_noep":
     data.to_string()
     if hasattr(data, 'list_episodes') and args.output != "full_noep":
-        print("\nEpisodes (" + str(data.episode_count) + ")")
+        print("Episodes (" + str(data.get_episode_count()) + ")")
         data.list_episodes()
 elif args.output == "episode_list":
     if hasattr(data, 'list_episodes'):
